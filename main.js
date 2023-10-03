a7bcbb63da08804649cb59805cf97814cf105716
rizk <rizkdotai@gmail.com>
2023-10-03 20:46:28 +0300

    Capture first card info dynamically

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 const button = document.getElementById("btn")
 const submit = document.getElementById("submit")
 const form = document.getElementById("form")
+const card = document.getElementById("card")
+const cardTitle = document.getElementById("card-title")
+const cardAuthor = document.getElementById("card-author")
+const cardPages = document.getElementById("card-pages")
+const cardStatus = document.getElementById("card-status")
+const cardRemove = document.getElementById("card-remove")
+
 
 const myLibrary = [];
 
@@ -21,9 +28,14 @@ function addBookToLibrary() {
     let status = document.querySelector("#status").checked
     let newBook = new Book(title, author, pages, status)
     myLibrary.push(newBook)
-    console.log(myLibrary)
+    displayBooks()
+
+    /*
+    cardTitle.textContent = myLibrary[0].title */
+    
+    /*console.log(myLibrary) */
     /* you need to append newBook to myLibrary */
-   
+    
 }
 
 
@@ -34,10 +46,51 @@ button.addEventListener("click", () => {
         form.style.display = 'none'
     }
 });
+const libraryContainer = document.getElementById("library-container")
 
 form.addEventListener("submit", function(event) {
    
     event.preventDefault()
     addBookToLibrary()
+
+    if (libraryContainer.style.display === 'none') {
+        libraryContainer.style.display = 'block';
+    } else {
+        libraryContainer.style.display = 'none'
+    }
     
 })
+
+/* write a function called display books that 
+dynamically creates and adds cards using JS */ 
+
+function displayBooks () {
+    const libraryContainer = document.getElementById("library-container")
+    libraryContainer.textContent = ""
+
+    myLibrary.forEach((book, index) => {
+        const bookCard = document.createElement("div")
+        bookCard.classList.add("book-card")
+
+        const titleElement = document.createElement("h2")
+        titleElement.textContent = book.title
+
+        const authorElement = document.createElement("p")
+        authorElement.textContent = book.author
+
+        const pagesElement = document.createElement("p")
+        pagesElement.textContent = book.pages
+
+        const statusElement = document.createElement("p")
+        statusElement.textContent = book.status ? "Read" : "Unread"
+       
+        bookCard.appendChild(titleElement)
+        bookCard.appendChild(authorElement)
+        bookCard.appendChild(pagesElement)
+        bookCard.appendChild(statusElement)
+
+        libraryContainer.appendChild(bookCard)
+        
+    }
+    )
+}
