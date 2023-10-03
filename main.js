a959f533e9c57979e1c02d1a4ef8e5f509104f7b
rizk <rizkdotai@gmail.com>
2023-10-03 14:03:17 +0300

    Add new book to array

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+const button = document.getElementById("btn")
+const submit = document.getElementById("submit")
+const form = document.getElementById("form")
+
 const myLibrary = [];
 
 function Book(title, author, pages, status) {
@@ -10,16 +15,29 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary() {
-    // do stuff here
+    let title = document.querySelector("#title").value
+    let author = document.querySelector("#author").value
+    let pages = document.querySelector("#pages").value
+    let status = document.querySelector("#status").checked
+    let newBook = new Book(title, author, pages, status)
+    myLibrary.push(newBook)
+    console.log(myLibrary)
+    /* you need to append newBook to myLibrary */
+   
 }
 
-const button = document.getElementById("btn")
 
 button.addEventListener("click", () => {
-    const form = document.getElementById("form")
     if (form.style.display === 'none') {
         form.style.display = 'block';
     } else {
         form.style.display = 'none'
     }
 });
+
+form.addEventListener("submit", function(event) {
+   
+    event.preventDefault()
+    addBookToLibrary()
+    
+})
